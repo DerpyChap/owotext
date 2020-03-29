429809790ac48b4297ae9beaae4242f10dadeda3
DerpyChap <holla@derpychap.co.uk>
2020-03-29 19:37:35 +0100

    Add setup scwipt

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md', encoding='utf8') as f:
+        return f.read()
+
+setup(name='owotext',
+      version='1.0.0',
+      description='A Python library for converting text strings into OwO',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/DerpyChap/owotext',
+      author='DerpyChap',
+      author_email='holla@derpychap.co.uk',
+      license='MIT',
+      packages=['owotext'],
+      include_package_data=True,
+      zip_safe=False)
