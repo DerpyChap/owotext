3ddebd20fa1fb178fad46b2a19ed898ccecab438
DerpyChap <holla@derpychap.co.uk>
2020-11-07 21:43:37 +0000

    <3 Bump vewsion numbew (　'◟ ')

--- owotext/__init__.py
+++ owotext/__init__.py
@@ -11,12 +11,11 @@ __title__ = 'owotext'
 __author__ = 'DerpyChap'
 __license__ = 'ISC'
 __copyright__ = 'Copyright 2020 DerpyChap'
-__version__ = '1.0.2'
+__version__ = '1.1.0'
 
 from collections import namedtuple
 from .owo import OwO
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
-version_info = VersionInfo(
-major=1, minor=0, micro=1, releaselevel='final', serial=0)
+version_info = VersionInfo(major=1, minor=1, micro=0, releaselevel='final', serial=0)
