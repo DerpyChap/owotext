0886885f6e0430ecf1288e6604a2a01423f9975f
DerpyChap <holla@derpychap.co.uk>
2020-11-07 21:40:29 +0000

    Huohhhh. Add comments and bastawdise hewp text ;_;

--- owotext/owo.py
+++ owotext/owo.py
@@ -78,6 +78,12 @@ class OwO:
         self.substitutions = _substitutions
 
     def whatsthis(self, text: str):
+        """
+        UwU Convewts da specified stwing into OwO speak ʕʘ‿ʘʔ
+
+        :param text: Huohhhh. Da text uu want to convewt..
+        :return: OWO Da convewted stwing (人◕ω◕)
+        """
         text = self.translate(text)
         if self.prefixes:
             text = (random.choice(self.prefixes) + text)
@@ -86,27 +92,46 @@ class OwO:
         return text
 
     def translate(self, text: str):
+        """
+        Convewts da specified stwing into OwO speak, without a pwefix ow suffix
+
+        :param text: Da text uu want to convewt
+        :return: Da convewted stwing
+        """
         for key, value in self.substitutions.items():
             text = text.replace(key, value)
         return text
 
 
+# noinspection PyProtectedMember
 def main():
-    parser = argparse.ArgumentParser(
-        description='A Python library for converting text strings into OwO',
-        epilog='https://github.com/DerpyChap/owotext')
+    o = OwO()
 
-    parser.add_argument('text', type=str, nargs='*', help='The text to OwO',
-                        default=["Please give me some text to OwO"])
+    parser = argparse.ArgumentParser(add_help=False,
+                                     description=o.whatsthis(
+                                         'A Python library for converting text strings into OwO speak'),
+                                     epilog='https://github.com/DerpyChap/owotext')
 
+    parser._positionals.title = o.translate(parser._positionals.title)
+    parser._optionals.title = o.translate(parser._optionals.title)
+
+    parser.add_argument('-t', '--translate', action='store_true',
+                        help=o.translate('translate the text without a prefix or suffix'))
+    parser.add_argument('text', type=str, nargs='+', help=o.whatsthis('the text to convert'))
+    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
+                        help=o.whatsthis('show this help message and exit'))
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
     args = parser.parse_args()
     text = ' '.join(args.text)
 
     if not sys.stdin.isatty():
         text = sys.stdin.read().rstrip()
-
-    o = OwO()
-    print(o.whatsthis(text))
+    if args.translate:
+        print(o.translate(text))
+    else:
+        print(o.whatsthis(text))
 
 
 if __name__ == '__main__':
